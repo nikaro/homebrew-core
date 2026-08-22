76b9768062068eaba9640375534972c11a476862
Michael Cho <michael@michaelcho.dev>
2026-08-22 00:02:44 -0400

    whisper.cpp 1.9.4 (renamed from whisper-cpp)

---
+++ Formula/w/whisper.cpp.rb
@@ -0,0 +1,72 @@
+class WhisperCpp < Formula
+  desc "Port of OpenAI's Whisper model in C/C++"
+  homepage "https://github.com/ggml-org/whisper.cpp"
+  url "https://github.com/ggml-org/whisper.cpp/archive/refs/tags/v1.9.4.tar.gz"
+  sha256 "57e280cee375ab02425b806ad5146b99f6eb9357e3c2b31357c8a6af2e2e44ae"
+  license "MIT"
+  compatibility_version 1
+  head "https://github.com/ggml-org/whisper.cpp.git", branch: "master"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  depends_on "cmake" => :build
+  depends_on "pkgconf" => :test
+  depends_on "ggml" # NOTE: reject all PRs that try to bundle ggml
+  depends_on "llama.cpp"
+  depends_on "sdl2-compat"
+
+  def install
+    args = %W[
+      -DBUILD_SHARED_LIBS=ON
+      -DCMAKE_INSTALL_RPATH=#{rpath}
+      -DWHISPER_SDL2=ON
+      -DWHISPER_BUILD_EXAMPLES=ON
+      -DWHISPER_BUILD_TESTS=OFF
+      -DWHISPER_BUILD_SERVER=OFF
+      -DWHISPER_USE_SYSTEM_GGML=ON
+      -DWHISPER_USE_SYSTEM_LLAMA=ON
+    ]
+    args << "-DWHISPER_BUILD_IS_DEV=OFF" if build.stable?
+
+    system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+    pkgshare.install "models/for-tests-ggml-tiny.bin", "samples/jfk.wav"
+  end
+
+  def caveats
+    <<~EOS
+      #{name} requires GGML model files to work. These are not downloaded by default.
+      To obtain model files (.bin), visit one of these locations:
+
+        https://huggingface.co/ggerganov/whisper.cpp/tree/main
+        https://ggml.ggerganov.com/
+    EOS
+  end
+
+  test do
+    model = pkgshare/"for-tests-ggml-tiny.bin"
+    output = shell_output("#{bin}/whisper-cli --model #{model} #{pkgshare}/jfk.wav 2>&1")
+    assert_match "processing '#{pkgshare}/jfk.wav' (176000 samples, 11.0 sec)", output
+
+    (testpath/"test.cpp").write <<~CPP
+      #include <whisper.h>
+      #include <cassert>
+      int main() {
+        ggml_backend_load_all();
+        struct whisper_context_params cparams = whisper_context_default_params();
+        struct whisper_context * ctx = whisper_init_from_file_with_params("#{model}", cparams);
+        assert(ctx != nullptr);
+        whisper_free(ctx);
+        return 0;
+      }
+    CPP
+
+    flags = shell_output("pkgconf --cflags --libs ggml whisper").chomp.split
+    system ENV.cxx, "-std=c++11", "test.cpp", "-o", "test", *flags
+    system "./test"
+  end
+end
