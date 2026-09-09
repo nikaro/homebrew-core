204b8a83081e401fb3dea765a72830d5b19c6bf6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-09 18:13:20 +0000

    steampipe 2.4.6

--- Formula/s/steampipe.rb
+++ Formula/s/steampipe.rb
@@ -1,8 +1,8 @@
 class Steampipe < Formula
   desc "Use SQL to instantly query your cloud services"
   homepage "https://steampipe.io/"
-  url "https://github.com/turbot/steampipe/archive/refs/tags/v2.4.5.tar.gz"
-  sha256 "1226068715e480e8faa0eff37134741898a05222b360d85bd794ed9eae2bd216"
+  url "https://github.com/turbot/steampipe/archive/refs/tags/v2.4.6.tar.gz"
+  sha256 "fa685e46d435a9eb59d3e69bf718bf19bfeac17764c616fa93fd9ce5c5d9cd80"
   license "AGPL-3.0-only"
   head "https://github.com/turbot/steampipe.git", branch: "develop"
 
@@ -22,9 +22,15 @@ class Steampipe < Formula
 
   depends_on "go" => :build
 
+  deny_network_access!
+
+  def fetch
+    system "go", "mod", "download"
+  end
+
   def install
     ldflags = "-X main.version=#{version} -X main.date=#{time.iso8601} -X main.commit=#{tap.user}"
-    system "go", "build", *std_go_args(ldflags:)
+    system "go", "build", *std_go_args(ldflags:, tags: "http2legacy")
 
     generate_completions_from_executable(bin/"steampipe", shell_parameter_format: :cobra)
   end
@@ -35,6 +41,6 @@ class Steampipe < Formula
     output = shell_output("#{bin}/steampipe service status")
     assert_match "Steampipe service is not installed", output
 
-    assert_match "Steampipe v#{version}", shell_output("#{bin}/steampipe --version")
+    assert_match version.to_s, shell_output("#{bin}/steampipe --version")
   end
 end
