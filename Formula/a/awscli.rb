e038d58c8f3eacfbcaae2e26352367be04383fa0
botantony <antonsm21@gmail.com>
2026-09-10 23:26:28 +0200

    awscli: revision bump (aws-c-s3 1.1.0)

--- Formula/a/awscli.rb
+++ Formula/a/awscli.rb
@@ -6,6 +6,7 @@ class Awscli < Formula
   url "https://github.com/aws/aws-cli/archive/refs/tags/2.36.43.tar.gz"
   sha256 "a2d2e2d3049116e598cc1d978f7eab9e2dfeca6815726220455effffe0bc9588"
   license "Apache-2.0"
+  revision 1
   compatibility_version 1
   head "https://github.com/aws/aws-cli.git", branch: "v2"
 
