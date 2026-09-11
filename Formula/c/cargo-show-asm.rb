6be72f55ef15a6bc39935c5a9c9c9583ff1df0b8
Anton Melnikov <antonsm21@gmail.com>
2026-09-11 11:01:41 +0000

    cargo-show-asm: update 0.2.62 bottle.

--- Formula/c/cargo-show-asm.rb
+++ Formula/c/cargo-show-asm.rb
@@ -7,12 +7,13 @@ class CargoShowAsm < Formula
   head "https://github.com/pacak/cargo-show-asm.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "b4d9be4aadb7d4d767cc20f97a4029490842b86a5f16f83aa2b0cf807ac72140"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "fb97375bd038261fd0f99772627925a325b979c861d985d91a86ae97a051c5c8"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ba4fd404ba4a5ec376614e3273fe3cb5c2ae75ddd8b1e9d4c23d73a4d2d96114"
-    sha256 cellar: :any_skip_relocation, sonoma:        "0af8329cf99d77c21e719aa61a8505452a40150a9a29963c36bf7e9121a58558"
-    sha256 cellar: :any,                 arm64_linux:   "f9b415ed141300e7f4e5111892ef5234b17488157224cff4de870745774ba6ea"
-    sha256 cellar: :any,                 x86_64_linux:  "a73752bb7daa48ec4d826e3c263c5f2385241900f5a071885890679274dde8a0"
+    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "35b52d11c1639112976c9f5bea4787be731691dc056ce5320282a108a989b64a"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "b4d9be4aadb7d4d767cc20f97a4029490842b86a5f16f83aa2b0cf807ac72140"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "fb97375bd038261fd0f99772627925a325b979c861d985d91a86ae97a051c5c8"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:      "ba4fd404ba4a5ec376614e3273fe3cb5c2ae75ddd8b1e9d4c23d73a4d2d96114"
+    sha256 cellar: :any_skip_relocation, sonoma:            "0af8329cf99d77c21e719aa61a8505452a40150a9a29963c36bf7e9121a58558"
+    sha256 cellar: :any,                 arm64_linux:       "f9b415ed141300e7f4e5111892ef5234b17488157224cff4de870745774ba6ea"
+    sha256 cellar: :any,                 x86_64_linux:      "a73752bb7daa48ec4d826e3c263c5f2385241900f5a071885890679274dde8a0"
   end
 
   depends_on "rust" => :build
