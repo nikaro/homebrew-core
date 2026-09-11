a501f37f2d95c3da354f0ca102dd404a1d888172
Anton Melnikov <antonsm21@gmail.com>
2026-09-11 15:54:34 +0000

    nx: update 23.2.1 bottle.

--- Formula/n/nx.rb
+++ Formula/n/nx.rb
@@ -7,11 +7,11 @@ class Nx < Formula
   version_scheme 1
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "a1e01ec75374837be985d6dd6248f4d44711945c51ba111ea4fb844ce5faff72"
-    sha256 cellar: :any,                 arm64_sequoia: "a1e01ec75374837be985d6dd6248f4d44711945c51ba111ea4fb844ce5faff72"
-    sha256 cellar: :any,                 arm64_sonoma:  "a1e01ec75374837be985d6dd6248f4d44711945c51ba111ea4fb844ce5faff72"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "a2524e766f4e7d3d65833cab13add01c8a1f17e19ebb06fefbdb8dff316a970a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "567163df6cad693ef53e36fed4320e25d39ee97e8b49f60ae76fc1c29b407ba9"
+    sha256 cellar: :any,                 arm64_golden_gate: "4acdadaa3fd1eaf9a3114dec39cf5ccf46670584ed70608f7e872c5b55f726a8"
+    sha256 cellar: :any,                 arm64_tahoe:       "4acdadaa3fd1eaf9a3114dec39cf5ccf46670584ed70608f7e872c5b55f726a8"
+    sha256 cellar: :any,                 arm64_sequoia:     "4acdadaa3fd1eaf9a3114dec39cf5ccf46670584ed70608f7e872c5b55f726a8"
+    sha256 cellar: :any_skip_relocation, arm64_linux:       "fefeff6f9f5c15b8c2b36c0f4eea6a3fbaba9f0a707c224f2fd36ef9e3fb23d4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:      "0dcc03262fa46dd356e04954e6e57e060258b9b5d3b755e5d0a2b8687a3176e2"
   end
 
   depends_on "node"
