670dd878becc57aa27762133144e81fb22f2099d
botantony <antonsm21@gmail.com>
2026-09-12 01:49:07 +0200

    qtquick3dphysics: use `formula_opt_*` methods instead of `Formula`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Formula/q/qtquick3dphysics.rb
+++ Formula/q/qtquick3dphysics.rb
@@ -122,6 +122,6 @@ class Qtquick3dphysics < Formula
 
     ENV["LC_ALL"] = "en_US.UTF-8"
     ENV["QT_QPA_PLATFORM"] = "minimal" if OS.linux?
-    system Formula["qtdeclarative"].bin/"qml", "test.qml"
+    system formula_opt_bin("qtdeclarative")/"qml", "test.qml"
   end
 end
