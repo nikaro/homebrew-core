49d7f091627c6a3f5d1e033f4b7a2f6ffc4f7713
botantony <antonsm21@gmail.com>
2026-09-11 15:29:28 +0200

    teleport: replace `Formula["rustup"].bin` with `formula_opt_bin("rustup")`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Formula/t/teleport.rb
+++ Formula/t/teleport.rb
@@ -65,7 +65,7 @@ class Teleport < Formula
       managePackageManagerVersions: false
     YAML
 
-    ENV.prepend_path "PATH", Formula["rustup"].bin
+    ENV.prepend_path "PATH", formula_opt_bin("rustup")
     system "rustup", "set", "profile", "minimal"
     system "rustup", "default", "stable"
 
