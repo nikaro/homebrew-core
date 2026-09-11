1ae9bfccfe5cf3cdaa589a5566df8d7c044e30c6
Michael Cho <michael@michaelcho.dev>
2026-09-10 23:11:37 -0400

    llvm@22: apply backport for macOS 27 SDK

--- Formula/l/llvm@22.rb
+++ Formula/l/llvm@22.rb
@@ -59,6 +59,12 @@ class LlvmAT22 < Formula
     type :backport
     resolves "https://github.com/llvm/llvm-project/pull/205044"
   end
+  patch do
+    url "https://github.com/llvm/llvm-project/commit/b8007a8e4020b8bca2b12e941660e10bf5bf6716.patch?full_index=1"
+    sha256 "e41e300eb6f5cca9172ab344e572c3fb24f0d05885ae23dd7cb4f9c2528839f7"
+    type :backport
+    resolves "https://github.com/llvm/llvm-project/pull/222721"
+  end
 
   def clang_config_file_dir
     etc/"clang"
