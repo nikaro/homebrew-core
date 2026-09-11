2a4d1c9b89c1367cd438b6976155057f3c7fe8e0
botantony <antonsm21@gmail.com>
2026-09-11 11:32:56 +0200

    recc: revision bump (grpc 1.84.0)

--- Formula/r/recc.rb
+++ Formula/r/recc.rb
@@ -4,6 +4,7 @@ class Recc < Formula
   url "https://gitlab.com/BuildGrid/buildbox/buildbox/-/archive/1.4.24/buildbox-1.4.24.tar.gz"
   sha256 "31b811d0e05a796b938e7371d59e23ebf888852a9a2e9dc76c9de93192cd42b7"
   license "Apache-2.0"
+  revision 1
   head "https://gitlab.com/BuildGrid/buildbox/buildbox.git", branch: "master"
 
   bottle do
