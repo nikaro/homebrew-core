bd0c44d51e666b86707a1c45390b93ee72d988d8
Anton Melnikov <antonsm21@gmail.com>
2026-09-11 09:44:27 +0000

    plink-ng: update 2.0.0-a.7.5 bottle.

--- Formula/p/plink-ng.rb
+++ Formula/p/plink-ng.rb
@@ -13,11 +13,11 @@ class PlinkNg < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_tahoe:   "2c938bae3db2ac57b0a02c473b4e84c2446f9d47c4def1511d44bfc47a90e56e"
-    sha256 cellar: :any, arm64_sequoia: "cebf9949bfed2c74805e614b72a50f6d8ecefbe0f11dff0ee4501a9004d8f870"
-    sha256 cellar: :any, arm64_sonoma:  "7de1a595536c9622e00c172c30f922bb689e35b826f4cc13f76b4883ea04d1d8"
-    sha256 cellar: :any, arm64_linux:   "62eae50f924b04b6dd3b363b15c7983cd6a787faed7d77e63de80ba090c2ae85"
-    sha256 cellar: :any, x86_64_linux:  "a103ff4f1dcb3fb17ba7593336dd9148183803aea72c31ce11429b1116ec8662"
+    sha256 cellar: :any, arm64_golden_gate: "fc114d175f81100a257bb63bd33b880cfacd60fd3e0adf474e3003c821a32337"
+    sha256 cellar: :any, arm64_tahoe:       "d0663e214fa35428be3a98c565e41de488bfe0a4e1f05b530e19fd75a6510f6c"
+    sha256 cellar: :any, arm64_sequoia:     "2bfa1699bffd886887cfdffba6de6944482b2a3ec5472d899d1f403931f5b492"
+    sha256 cellar: :any, arm64_linux:       "884d6520d8dad01a89129a061f60588b9a0ffb43777726d367879c09ebd31f7a"
+    sha256 cellar: :any, x86_64_linux:      "6fb2d7e39f97e2a96d127706402ac9f9be528eabbb1226ba95fcc090974fc197"
   end
 
   depends_on "zstd"
