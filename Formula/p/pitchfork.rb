678a86686a6c141eb9e36cb0c8689385bb1ab58f
Anton Melnikov <antonsm21@gmail.com>
2026-09-11 13:13:08 +0000

    pitchfork: update 2.24.2 bottle.

--- Formula/p/pitchfork.rb
+++ Formula/p/pitchfork.rb
@@ -7,11 +7,12 @@ class Pitchfork < Formula
   head "https://github.com/jdx/pitchfork.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "46a80525d1b62b1b414a899dc3f443a0bf34fcb483409e8e458e75a0d928247c"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b54fb867cd4319a02b9ae0b3be07530ffff3028e640921fdf6f0739336c567b8"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "cb447ddda71dbf128eb205f602f0c53e8ac7c0ed92819c50661b56e28ff2ee82"
-    sha256 cellar: :any,                 arm64_linux:   "f9c79fb6c927b891c68d1f6a313ee8d4992c740ad27901e1fa6015ae1452e91d"
-    sha256 cellar: :any,                 x86_64_linux:  "b33dbc1c3d0c2119d53aa0bf7b66b33ee85ec3b1d30d2789b9a0d00e3972566d"
+    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "242367e7e61a02f2cebffebf873bac89d9fcedda13e1c415ffe354826254a876"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "46a80525d1b62b1b414a899dc3f443a0bf34fcb483409e8e458e75a0d928247c"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "b54fb867cd4319a02b9ae0b3be07530ffff3028e640921fdf6f0739336c567b8"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:      "cb447ddda71dbf128eb205f602f0c53e8ac7c0ed92819c50661b56e28ff2ee82"
+    sha256 cellar: :any,                 arm64_linux:       "f9c79fb6c927b891c68d1f6a313ee8d4992c740ad27901e1fa6015ae1452e91d"
+    sha256 cellar: :any,                 x86_64_linux:      "b33dbc1c3d0c2119d53aa0bf7b66b33ee85ec3b1d30d2789b9a0d00e3972566d"
   end
 
   depends_on "node" => :build
