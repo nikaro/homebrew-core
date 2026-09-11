5c1ad0e5f154b6a4797b4011d8ffa7b753c9730f
botantony <antonsm21@gmail.com>
2026-09-12 01:49:09 +0200

    qtvirtualkeyboard: use `formula_opt_*` methods instead of `Formula`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Formula/q/qtvirtualkeyboard.rb
+++ Formula/q/qtvirtualkeyboard.rb
@@ -80,6 +80,6 @@ class Qtvirtualkeyboard < Formula
 
     ENV["LC_ALL"] = "en_US.UTF-8"
     ENV["QT_QPA_PLATFORM"] = "minimal" if OS.linux?
-    system Formula["qtdeclarative"].bin/"qml", "test.qml"
+    system formula_opt_bin("qtdeclarative")/"qml", "test.qml"
   end
 end
