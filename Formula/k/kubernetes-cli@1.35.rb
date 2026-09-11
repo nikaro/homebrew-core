b36a98dc7a94e69282f9ba3dc3d4020bf9f6c549
botantony <antonsm21@gmail.com>
2026-09-12 01:49:01 +0200

    kubernetes-cli@1.35: use `formula_opt_*` methods instead of `Formula`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Formula/k/kubernetes-cli@1.35.rb
+++ Formula/k/kubernetes-cli@1.35.rb
@@ -33,7 +33,7 @@ class KubernetesCliAT135 < Formula
   end
 
   def install
-    ENV.prepend_path "PATH", Formula["coreutils"].libexec/"gnubin" if OS.mac? # needs GNU date
+    ENV.prepend_path "PATH", formula_opt_libexec("coreutils")/"gnubin" if OS.mac? # needs GNU date
     ENV["FORCE_HOST_GO"] = "1"
     system "make", "WHAT=cmd/kubectl"
     bin.install "_output/bin/kubectl"
