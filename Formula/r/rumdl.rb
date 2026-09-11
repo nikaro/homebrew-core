e3f58c0a7a9de11b25adc338242776571918c74f
Anton Melnikov <antonsm21@gmail.com>
2026-09-11 18:35:29 +0000

    rumdl: update 0.2.72 bottle.

--- Formula/r/rumdl.rb
+++ Formula/r/rumdl.rb
@@ -11,11 +11,11 @@ class Rumdl < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "87b47988272c391abb5a209fc3457719ce42271a38f424bea5bd9f55faad888a"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4534b98b817265c302b5dcf4e6a8b1c6ead2a2a873ee1271fc8923c437f5c7f1"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b1557ffd75cfb9fa8afc15c4e5062bcb59fcff780a41de9cc1acf4bb00f2e6c3"
-    sha256 cellar: :any,                 arm64_linux:   "97c092e0a37bf78cdafb9d91920ebe7b9c575cd4b7f4d6467422e1f8f0b4dbf3"
-    sha256 cellar: :any,                 x86_64_linux:  "0cb7090b12eba747f2a34006c8e14d686d0e1ddcb61b550f64c01766039bd67f"
+    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "679fb0ce6bec2efad04edd03324257bb6422a5bad333be078991803b73098eb4"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "a036703a597c2f06b180b3a77f445287ccbb21497bad22b995bdac08bb2fb5a1"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "59625f28d052ad22aad38789487e4f672b1dde7f8df255e56045600ff6fd4f40"
+    sha256 cellar: :any,                 arm64_linux:       "4adce7a7c4682fd813c5579a3954ac986f07f8faec7226bcb3bf5086f8186743"
+    sha256 cellar: :any,                 x86_64_linux:      "c8270289104b7197ca15d7893250fed64e37bfd979d6864e4c25e355d1b45e4a"
   end
 
   depends_on "rust" => :build
