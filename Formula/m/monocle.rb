47eadb07e137d728654dde4165ff5e331a78eb6d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-11 02:35:15 +0000

    monocle 1.6.0

--- Formula/m/monocle.rb
+++ Formula/m/monocle.rb
@@ -1,8 +1,8 @@
 class Monocle < Formula
   desc "See through all BGP data with a monocle"
   homepage "https://github.com/bgpkit/monocle"
-  url "https://github.com/bgpkit/monocle/archive/refs/tags/v1.5.0.tar.gz"
-  sha256 "0c0253533ab4a99cdc0fb825b6390ace9eb7f4fb27e6bc23d151ef98b630422b"
+  url "https://github.com/bgpkit/monocle/archive/refs/tags/v1.6.0.tar.gz"
+  sha256 "1940d5c880a18e1839d327a87abdf29c3ec5ab95c45fcfd76b71f60ae2274ec5"
   license "MIT"
 
   bottle do
