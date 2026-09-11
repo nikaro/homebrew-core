a6f3cc631e1aff5c63583d492163ac8716ab3490
botantony <antonsm21@gmail.com>
2026-09-11 13:32:27 +0200

    cdrdao: remove patch
    This patch was not accepted, contains invalid UTF-8 sequence, and used
    only to support Tier-3 macOS versions
    
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Formula/c/cdrdao.rb
+++ Formula/c/cdrdao.rb
@@ -30,15 +30,6 @@ class Cdrdao < Formula
   depends_on "libvorbis"
   depends_on "mad"
 
-  # Fixes build on macOS prior to 12.
-  # Remove when merged and released.
-  patch do
-    url "https://github.com/cdrdao/cdrdao/commit/105d72a61f510e3c47626476f9bbc9516f824ede.patch?full_index=1"
-    sha256 "0e235c0c34abaad56edb03a2526b3792f6f7ea12a8144cee48998cf1326894eb"
-    type :unofficial
-    resolves "https://github.com/cdrdao/cdrdao/pull/19"
-  end
-
   def install
     system "./autogen.sh"
     system "./configure", "--mandir=#{man}", *std_configure_args
