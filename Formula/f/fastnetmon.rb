85eac8bdf6d2f358b4f8eab30ea82ab2292b9266
botantony <antonsm21@gmail.com>
2026-09-11 11:32:53 +0200

    fastnetmon: revision bump (grpc 1.84.0)

--- Formula/f/fastnetmon.rb
+++ Formula/f/fastnetmon.rb
@@ -4,7 +4,7 @@ class Fastnetmon < Formula
   url "https://github.com/pavel-odintsov/fastnetmon/archive/refs/tags/v1.2.9.tar.gz"
   sha256 "5ecc10791af04fc1fd720a9a113060668426aa798d5b6c3921364213a31a5e9b"
   license "GPL-2.0-only"
-  revision 10
+  revision 11
 
   bottle do
     sha256 cellar: :any, arm64_tahoe:   "1322f1394239b6983c857446ef580fa161012b86fcad26b4b762cfc6cbc052cf"
