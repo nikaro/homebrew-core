b89f7f081d640ecdd90320bf2c7c99be1ab6c331
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-11 14:47:12 +0000

    kosli-cli 2.40.1

--- Formula/k/kosli-cli.rb
+++ Formula/k/kosli-cli.rb
@@ -1,8 +1,8 @@
 class KosliCli < Formula
   desc "CLI for managing Kosli"
   homepage "https://docs.kosli.com"
-  url "https://github.com/kosli-dev/cli/archive/refs/tags/v2.40.0.tar.gz"
-  sha256 "326bbcd0954b5e6cffacf5ab7251d0fac85ff2a4c6f3c9bf2e0c20e6d0b3fb07"
+  url "https://github.com/kosli-dev/cli/archive/refs/tags/v2.40.1.tar.gz"
+  sha256 "af484e41da4fe3750aef121eb5350d9f585fd4b1a31209f47ba833ffca999b75"
   license "MIT"
   head "https://github.com/kosli-dev/cli.git", branch: "main"
 
