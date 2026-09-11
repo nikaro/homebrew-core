5a96e320b89c99704f4c2c4e467adb0dc3d4cb34
Patrick Linnane <patrick@linnane.io>
2026-09-11 03:47:01 +0000

    mise: update 2026.9.5 bottle.

--- Formula/m/mise.rb
+++ Formula/m/mise.rb
@@ -12,11 +12,11 @@ class Mise < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "70f2c37e8e02dcb73f913f3a32f1c1d0c76c75dffdcc15fc4ddb61ff581f6fd3"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "21bf65d10920badadcf4f0f30a835acc0abf0956f694cf3593082cf6f7036e0e"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "bc12dc1d4b1d5ebe36ef05031d6c7efa1b732b1d2e6f9f3451c9ba9ce4408c85"
-    sha256 cellar: :any,                 arm64_linux:   "9e12cf45f24cc8aeafdbb9b62279e818c5a1b8c05a3058897433cc7199733cd9"
-    sha256 cellar: :any,                 x86_64_linux:  "28d4ad0054d357a4e1659e55846f79eea71eae3852d247a07ae4f34f4a247348"
+    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "24954218ee3c782d841f9a19a7c0c4fe1d265b423c9fee286a60abad5c7fc5d0"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "10094f45663e7ab1b76938777f23fc72ab8870eebf5083a5a4ecd3f05f3f4a78"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "08e7190866e70fec75571cb955dea29b9f3d630fc3e95c695e77c91ea0a64606"
+    sha256 cellar: :any,                 arm64_linux:       "1294c0698a89db45b0fd7f7b461bdc1c713e1e87a9c6b29e3882d0f2a66bd7d3"
+    sha256 cellar: :any,                 x86_64_linux:      "26b1d6dafbd589463d8df2d061762a0bc0ee9f2dc1a0de37f4bc97c4f17f808a"
   end
 
   depends_on "cmake" => :build
