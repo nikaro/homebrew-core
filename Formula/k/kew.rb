20d1c074dc015e00d405e6029694cf00b2089e4f
Patrick Linnane <patrick@linnane.io>
2026-09-12 05:42:46 +0000

    kew: update 4.3.3 bottle.

--- Formula/k/kew.rb
+++ Formula/k/kew.rb
@@ -7,12 +7,11 @@ class Kew < Formula
   head "https://github.com/ravachol/kew.git", branch: "main"
 
   bottle do
-    sha256 arm64_golden_gate: "a1f7144b08f1de3d0482d29ca4d5f383596e1317f50ff17253fd4d7301d788cf"
-    sha256 arm64_tahoe:       "485d60ea535b3af8804b0db96bebd65e1c5e8aba2934bd15863cdcdce004316a"
-    sha256 arm64_sequoia:     "453feaf474d0c44efef0750cfa744414f94473e0d79de4d52f793c4a98117d40"
-    sha256 arm64_sonoma:      "7f6c3104255b573507b5dacef976833830e8ff95c6bb446eabc4301926d51d22"
-    sha256 arm64_linux:       "9b53b0159d816a12102fb10f6698dccfc04e12d58ea61b810d75326ce1cc5270"
-    sha256 x86_64_linux:      "759595ac843e9643333b207eb3eb5516f0212c968fee1c1f0818ab3f0a669940"
+    sha256 arm64_golden_gate: "81f777c324b216b12798c94c669cb0f6702a967d0c44636ba28addad35b3b2d3"
+    sha256 arm64_tahoe:       "e55cd20cbe4beeb70c89afaef1858a72f17a6afcfa20b0d0f8fd950402c3ae42"
+    sha256 arm64_sequoia:     "9af0016fbbd80a86e0883bb4d51383ea06bf597380bd3450fac9e76d3e45fed4"
+    sha256 arm64_linux:       "a3cfc1b9705125df70a039cc4557d65daaaa654cd1c3443ac1b27556f159f019"
+    sha256 x86_64_linux:      "60a34047205774d865bbdaa7360fea177f2b69ba165708f48f455106012a0bae"
   end
 
   depends_on "pkgconf" => :build
