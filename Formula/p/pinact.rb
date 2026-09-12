5ca3b2c4456c32807b492302bfe5ff2494e94cc2
Patrick Linnane <patrick@linnane.io>
2026-09-12 06:03:14 +0000

    pinact: update 5.0.0 bottle.

--- Formula/p/pinact.rb
+++ Formula/p/pinact.rb
@@ -14,13 +14,11 @@ class Pinact < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "03f49dc6a8584f42d0448cf0829e5e94b6cd987144aca22aedf2e0e7d9d1fd82"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "ed29f6b7deee2751894f26251c135d041c514acb82ec81c3f96e470ffde81cd0"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "ed29f6b7deee2751894f26251c135d041c514acb82ec81c3f96e470ffde81cd0"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:      "ed29f6b7deee2751894f26251c135d041c514acb82ec81c3f96e470ffde81cd0"
-    sha256 cellar: :any_skip_relocation, sonoma:            "135d05fd0317ee449a4e8ca9321b261b60178da5da7c6a8fd8a7c964e055384a"
-    sha256 cellar: :any_skip_relocation, arm64_linux:       "60b1ec0ea7fc14c3f6715a2e9c7964ba101ac2b778e03dd2cd3823b7ef31aae2"
-    sha256 cellar: :any,                 x86_64_linux:      "3215e646539080eb1053bacdb3519fabe1169ff0a54de621b8136676ab5de18f"
+    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "e5bc3704f75de4338ac7b615649439d9efffd39b124976ff699aa4c0016ba1cc"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "e5bc3704f75de4338ac7b615649439d9efffd39b124976ff699aa4c0016ba1cc"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "e5bc3704f75de4338ac7b615649439d9efffd39b124976ff699aa4c0016ba1cc"
+    sha256 cellar: :any_skip_relocation, arm64_linux:       "7bc8138de3da4e2e2f525fea3ab70862136cddce749d12782e099f6cb40c5148"
+    sha256 cellar: :any,                 x86_64_linux:      "cb54357d3a2b4f6d7b2c0adc02df5cb1297235b562d0fafb9428e17a1dcca5a8"
   end
 
   depends_on "go" => :build
