e5c0a3ec95cba617890edd03735cd814e168b7a6
Patrick Linnane <patrick@linnane.io>
2026-09-12 05:40:22 +0000

    infisical: update 0.43.132 bottle.

--- Formula/i/infisical.rb
+++ Formula/i/infisical.rb
@@ -7,11 +7,11 @@ class Infisical < Formula
   head "https://github.com/Infisical/cli.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "2f83e546eeeab910b42218ca455c54dfc974092a264481936331581ecc864934"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "2f83e546eeeab910b42218ca455c54dfc974092a264481936331581ecc864934"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "2f83e546eeeab910b42218ca455c54dfc974092a264481936331581ecc864934"
-    sha256 cellar: :any_skip_relocation, arm64_linux:       "0541a83cfdf839dd66651b14dda75f0e4b376eca5d7017322939baa26b40bd10"
-    sha256 cellar: :any,                 x86_64_linux:      "0d5701cedfcf49a50ae206edb0786766f54791957b8b88768782ac66b6a56085"
+    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "f3fcf82285a6798a47da9fc802b7c77f18787a5e3aeec18f8e0fefd931a2540e"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "f3fcf82285a6798a47da9fc802b7c77f18787a5e3aeec18f8e0fefd931a2540e"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "f3fcf82285a6798a47da9fc802b7c77f18787a5e3aeec18f8e0fefd931a2540e"
+    sha256 cellar: :any_skip_relocation, arm64_linux:       "ade1cf3e75e0da395ef076c4e83b38b2b38097548bf1e960c87ce3361b909637"
+    sha256 cellar: :any,                 x86_64_linux:      "59439e57d6318de08d7cded5fc5c6060521d372712db359047d40eb41b64f3cf"
   end
 
   depends_on "go" => :build
