156da3083d594aad9d38787b6ef94694b978afa2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-12 18:27:22 +0000

    blastem: update 1.0.0 bottle.

--- Formula/b/blastem.rb
+++ Formula/b/blastem.rb
@@ -20,9 +20,11 @@ class Blastem < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 sonoma:       "06f0bb0d5d0e04267ab21bfa35f2146be7cf7c6acf85d8bd1a9141679c3741a1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "482c2321fde2beccb9a1d3ba301b9f4070a57306991c7eaa486137e646c1a9dc"
+    sha256 cellar: :any, arm64_golden_gate: "1d3be837a182c9c3794f99f8d05a56d9b664dff177a46012b43ae59210f3abac"
+    sha256 cellar: :any, arm64_tahoe:       "3ada7ac311478d5f37fbccd77cd14c9c9eda18276e700b81592f81e09f43a72b"
+    sha256 cellar: :any, arm64_sequoia:     "7fa084989f2845f425d2a29ae96ffb2e583a2b4bf7f971826af914312eeff0d8"
+    sha256 cellar: :any, arm64_linux:       "501d1b152dee4f26e7bd3a7139dcbc553a1ea5af6bddde4562340351dd014597"
+    sha256 cellar: :any, x86_64_linux:      "cbbb133a9566d6c825815d1cd85ae45c33b8bb16121ab303d72553536f2293a8"
   end
 
   depends_on "imagemagick" => :build
