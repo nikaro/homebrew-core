bad1e3c6740d1cf3df18c75d15c35cac554c120d
FX Coudert <1980544+fxcoudert@users.noreply.github.com>
2026-09-12 08:03:48 +0000

    oh-my-agent: update 14.7.9 bottle.

--- Formula/o/oh-my-agent.rb
+++ Formula/o/oh-my-agent.rb
@@ -6,11 +6,11 @@ class OhMyAgent < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any, arm64_tahoe:   "183b4bc5540748d1445691f775a209b3f791f8549903e6ad23178780ebaab17d"
-    sha256 cellar: :any, arm64_sequoia: "8fa089e3c04d0d1a935b14baf6e4fc35bc715efbbdfb39e9b1bf60b4032c3704"
-    sha256 cellar: :any, arm64_sonoma:  "ed9ddd435c90ba4028610a90d76077143374bb9a2c9fd19ef069dae3b38af499"
-    sha256 cellar: :any, arm64_linux:   "81974e2bd7d9663acd7e6b0fa2075a4d98d73960efa78b955c72d2ab7ea54e2b"
-    sha256 cellar: :any, x86_64_linux:  "3eabf1f060671adc5bbb561fa7d4f3fd85b83e1a7ad85d845e641b254c3a35ea"
+    sha256 cellar: :any, arm64_golden_gate: "c4cc21b44b2b4a6ad94dc6b25a1ee67a61600b6118c48b4b69c27243db244c7d"
+    sha256 cellar: :any, arm64_tahoe:       "f822fd767e290cd487b436fd7e50e5605040b34f90ab50a7f6b5644643e027d0"
+    sha256 cellar: :any, arm64_sequoia:     "7472939f95deff3cc0cc4245d50548947f7e75ac760e667dcdda528be309245d"
+    sha256 cellar: :any, arm64_linux:       "687138676f1c470378b5f8242424e5adc8e3a174a469df9c6e28498662e2bc51"
+    sha256 cellar: :any, x86_64_linux:      "6cda1fbd9d6148ae6177560bb0f585060917e4a9e5f770b4965c9a882fedb038"
   end
 
   depends_on "node"
