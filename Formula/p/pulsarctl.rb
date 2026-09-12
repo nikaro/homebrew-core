4b4e23478463883066efc845accdee3a4568b2a9
Patrick Linnane <patrick@linnane.io>
2026-09-12 06:09:26 +0000

    pulsarctl: update 4.2.1.15 bottle.

--- Formula/p/pulsarctl.rb
+++ Formula/p/pulsarctl.rb
@@ -19,11 +19,11 @@ class Pulsarctl < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "380327d4db6dae79fbe0007f2777afa197525d10e892d5fa51d8a7542c851f1e"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "380327d4db6dae79fbe0007f2777afa197525d10e892d5fa51d8a7542c851f1e"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "380327d4db6dae79fbe0007f2777afa197525d10e892d5fa51d8a7542c851f1e"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "dcc0054bfe51bc936e0b4a80f44c65604f87d54f790894f7680c7987adea3775"
-    sha256 cellar: :any,                 x86_64_linux:  "da34f854c7eda2823b557f870bd2de65a21e3b3a254bd59db21d160afac80173"
+    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "942a5821f2d21f33ccece43d21f308641966d56b66fc535e4302aab4212bd27f"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "942a5821f2d21f33ccece43d21f308641966d56b66fc535e4302aab4212bd27f"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "942a5821f2d21f33ccece43d21f308641966d56b66fc535e4302aab4212bd27f"
+    sha256 cellar: :any_skip_relocation, arm64_linux:       "9a8d3a54fdd622803253d529cde310205c37c4e1596572b5d1ac5ab8b1d8f5dd"
+    sha256 cellar: :any,                 x86_64_linux:      "8e97bd8823b9cb97c1a2cf1ee612c995021fa4c30457eb28b51377ed9de2e4cd"
   end
 
   depends_on "go" => :build
