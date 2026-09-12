cbe543b628cd96fe3e262420f1ccce27a1869a10
Ruoyu Zhong <zhongruoyu@outlook.com>
2026-09-12 08:53:14 +0000

    b4n: update 0.5.7 bottle.

--- Formula/b/b4n.rb
+++ Formula/b/b4n.rb
@@ -7,11 +7,12 @@ class B4n < Formula
   head "https://github.com/fioletoven/b4n.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "0fc8fbbb891dbdb72330e1de46d358848fafbf3ba014d67aa0fbeef67959b345"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "01b39d277c6dcb63b22e09f35f670484123da57c30eb807a2510b07cc9792d89"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0245557a655c9e8c2997c9591b1011ef2b4f156248a446002421d50380e4ea49"
-    sha256 cellar: :any,                 arm64_linux:   "fe1cc82f26eec4a44a77b3430577cde53cfc0b9931b70c50e3d428a7fd19fdc4"
-    sha256 cellar: :any,                 x86_64_linux:  "ed4f898c000499c4c400908cab1d7f2e02fce40d3b5cc2021423823e5a38a23e"
+    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "c2ed7c0a3f7bea45f76a99f95bd981fe424e514ce44cecb891cdbefeda4790ad"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "0fc8fbbb891dbdb72330e1de46d358848fafbf3ba014d67aa0fbeef67959b345"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "01b39d277c6dcb63b22e09f35f670484123da57c30eb807a2510b07cc9792d89"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:      "0245557a655c9e8c2997c9591b1011ef2b4f156248a446002421d50380e4ea49"
+    sha256 cellar: :any,                 arm64_linux:       "fe1cc82f26eec4a44a77b3430577cde53cfc0b9931b70c50e3d428a7fd19fdc4"
+    sha256 cellar: :any,                 x86_64_linux:      "ed4f898c000499c4c400908cab1d7f2e02fce40d3b5cc2021423823e5a38a23e"
   end
 
   depends_on "rust" => :build
